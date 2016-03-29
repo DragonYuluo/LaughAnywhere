742a9c47877054d728e79acd2dbec08921ef005d
DragonYuluo <330755661@qq.com>
2016-03-29 16:05:32 +0800

    Dragon 1.0.0

---
+++ LaughAnywhere/Common/LaughAnywhereAPI.h
@@ -0,0 +1,25 @@
+//
+//  LaughAnywhereAPI.h
+//  LaughAnywhere
+//
+//  Created by 龙SIR on 16/3/29.
+//  Copyright © 2016年 longliming. All rights reserved.
+//
+
+#ifndef LaughAnywhereAPI_h
+#define LaughAnywhereAPI_h
+
+
+//  段子
+#define kJokerAPI "http://xiaoliao.vipappsina.com/index.php/Api386/index/pad/0/sw/1/cid/joke/p/1/markId/0/date/"
+
+//  趣图
+#define kLaughAPI "http://xiaoliao.vipappsina.com/index.php/Api386/index/pad/0/sw/1/cid/qutu/p/1/markId/0/date/"
+
+//  物语
+#define kStoryAPI "http://xiaoliao.vipappsina.com/index.php/Api386/index/pad/0/sw/1/cid/mm/p/1/markId/0/date/"
+
+//  视屏
+#define kVideoAPI "http://xiaoliao.vipappsina.com/index.php/Api386/index/pad/0/sw/1/cid/video/p/1/markId/0/date/"
+
+#endif /* LaughAnywhereAPI_h */
---
+++ LaughAnywhere/Common/PorjDefine.h
@@ -0,0 +1,16 @@
+//
+//  PorjDefine.h
+//  LaughAnywhere
+//
+//  Created by 龙SIR on 16/3/29.
+//  Copyright © 2016年 longliming. All rights reserved.
+//
+
+#ifndef PorjDefine_h
+#define PorjDefine_h
+
+//  屏幕宽高
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
+
+#endif /* PorjDefine_h */
